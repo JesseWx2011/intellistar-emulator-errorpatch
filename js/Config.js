a8c45a45095aa881a9fc3cc80d1b61a3c3218e9e
Jesse Hasty <134172009+JesseWx2011@users.noreply.github.com>
2024-11-24 09:50:50 -0600

    Update Config.js

--- js/Config.js
+++ js/Config.js
@@ -6,8 +6,8 @@ window.CONFIG = {
   units: 'e', // Supported in TWC API (e = English (imperial), m = Metric, h = Hybrid (UK)),
   unitField: 'imperial', // Supported in TWC API. This field will be filled in automatically. (imperial = e, metric = m, uk_hybrid = h)
   loop: false,
-  background: "11" // Options are 1-17, find the backgrounds in assets/backgrounds
-  locationMode: "POSTAL",'
+  background: '11', // Options are 1-17, find the backgrounds in assets/backgrounds
+  locationMode: "POSTAL",
   secrets: {
     // Possibly deprecated key: See issue #29
     // twcAPIKey: 'd522aa97197fd864d36b418f39ebb323'
